6fb2b060bf6a611a656d7ad7205423aebbaf3e99
crydestaahl <kristoffer@oholics.com>
2021-04-20 08:36:32 +0200

    More styling.

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,10 +26,21 @@ const IndexPage = () => (
             <h3>Nyheter</h3>
         </div>
         <div className="blog-posts">
-          BLOG POST GOES HERE
+        
+          
+
+
+
+        </div>
+        <div className="blog">
+          <div className="blog-title">
+            <h3>Om Klubben</h3>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
         </div>
       </div>      
-    </div>
+    
 
     
   </Layout>
